6b22d723a55c56c29be7b6b2ad79f362aaace15d
Lucas ABM <lucasabmacedo@hotmail.com>
2021-05-02 00:18:08 -0300

    Atualiza fdash e bola

--- r2a/r2abola.py
+++ r2a/r2abola.py
@@ -17,47 +17,33 @@ from r2a.ir2a import IR2A
 import time
 from statistics import mean
 import numpy
-import json
 
 class R2ABola(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
-        self.throughputs = []
         self.parsed_mpd = ''
         self.qi = []
+        self.maxBufferSize = self.whiteboard.get_max_buffer_size()
 
     def handle_xml_request(self, msg):
-        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
 
-        t = time.perf_counter() - self.request_time
-        self.throughputs.append(msg.get_bit_length() / t)
-
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-        self.request_time = time.perf_counter()
-
         pbs = self.whiteboard.get_playback_buffer_size()
         if (len(pbs) == 0):
             bufferSize = 0
         else:
             bufferSize = pbs[-1][1]
 
-        a = 0
-        if (bufferSize == 59):
-            a = 1
-
-        f = open('dash_client.json')
-        maxBufferSize = json.load(f)['max_buffer_size']
-
         fator = 5
-        control = maxBufferSize/(numpy.log(self.qi[19] / self.qi[0]) + fator)
+        control = self.maxBufferSize/(numpy.log(self.qi[19] / self.qi[0]) + fator)
         maior = 0
         indiceMaior = 0
 
@@ -70,29 +56,6 @@ class R2ABola(IR2A):
 
         msg.add_quality_id(self.qi[indiceMaior])
         self.send_down(msg)
-
-        # utility = numpy.log(self.qi[19] / self.qi[0])
-        # control = 59/(utility + 5) # v
-        # # control * utility + control * 5 - 
-        # bufferSize = 0
-        # pbs = self.whiteboard.get_playback_buffer_size()
-        # if (len(pbs) == 0):
-        #     bufferSize = 0
-        # else:
-        #     bufferSize = self.whiteboard.get_playback_buffer_size()[-1][1]
-
-        # print(bufferSize)
-
-        # avg = mean(self.throughputs) / 2
-
-        # selected_qi = self.qi[0]
-        # for i in self.qi:
-        #     if avg > i:
-        #         selected_qi = i
-
-        # # define a qualidade selecionada, provavelmente não iremos mudar
-        # msg.add_quality_id(selected_qi)
-        # self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
         self.send_up(msg)
--- r2a/r2afdash.py
+++ r2a/r2afdash.py
@@ -25,53 +25,34 @@ class R2AFDASH(IR2A):
     def __init__(self, id):
         IR2A.__init__(self, id)
         self.timeToDownload = 0
-        self.sizeList = []
+        self.segmentSize = 0
         self.riList = []
-        self.parsed_mpd = ''
         self.qi = []
 
-        self.f = open('dash_client.json')
-        self.config = json.load(self.f)
-        self.maxBufferSize = self.config['max_buffer_size']
-        self.stepSize = self.config['playbak_step']       
-        self.d = 10  # tal
-
-        self.n1 = 0.25
-        self.n2 = 0.5
-        self.z  = 1
-        self.t1 = 1.5
-        self.t2 = 2
+        f = open('dash_client.json')
+        self.stepSize = json.load(f)['playbak_step']       
+        self.maxBufferSize = self.whiteboard.get_max_buffer_size()
+        self.d = 20  # tal
 
     def handle_xml_request(self, msg):
-        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
         parsed_mpd = parse_mpd(msg.get_payload())
         self.qi = parsed_mpd.get_qi()
 
-        t = time.perf_counter() - self.request_time
-        # self.throughputs.append(msg.get_bit_length() / t)
-
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-        self.request_time = time.perf_counter()
-
         pbs = self.whiteboard.get_playback_buffer_size()
         if (len(pbs) == 0):
             bufferSize = 0
         else:
             bufferSize = pbs[-1][1]
 
-        if (len(self.sizeList) != 0):
-          # limita a sizeList para tamanho 10
-          while (len(self.sizeList) > 10):
-            self.sizeList.pop(-1)
+        if (self.segmentSize != 0):
           
-          bi = self.sizeList[0]
-          ri = (bi * self.stepSize) / self.timeToDownload
-          
+          ri = (self.segmentSize * self.stepSize) / self.timeToDownload
           self.riList.insert(0, (ri, time.perf_counter(), bufferSize))
           
           currentTime = time.perf_counter()
@@ -151,9 +132,11 @@ class R2AFDASH(IR2A):
         else:
           chosenQuality = self.qi[0]
 
-        self.sizeList.insert(0, chosenQuality)
+
+        self.segmentSize = chosenQuality
         msg.add_quality_id(chosenQuality)
 
+        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
